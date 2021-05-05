40eae9e8cd155cfccb8785dd1df9f13bddebeb0b
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-05 20:22:30 -0300

    Adding some new page elements

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,91 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Seller</th>
+                        <th>Customers visited</th>
+                        <th>Closed deals</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>44</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Clark Kent</td>
+                        <td>24</td>
+                        <td>17</td>
+                        <td>8012.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Bruce Wayne</td>
+                        <td>34</td>
+                        <td>34</td>
+                        <td>35000.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Hal Jordan</td>
+                        <td>30</td>
+                        <td>20</td>
+                        <td>10013.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Diana</td>
+                        <td>22</td>
+                        <td>18</td>
+                        <td>9000.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Logan</td>
+                        <td>15</td>
+                        <td>2</td>
+                        <td>3500.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Charles Xavier</td>
+                        <td>40</td>
+                        <td>35</td>
+                        <td>17500.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Steve Rogers</td>
+                        <td>50</td>
+                        <td>50</td>
+                        <td>65000.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Bruce Banner</td>
+                        <td>05</td>
+                        <td>05</td>
+                        <td>7000.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Peter Parker</td>
+                        <td>31</td>
+                        <td>29</td>
+                        <td>30200.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://github.com/LeoManzini" target="_blank" rel="noreferrer">Leonardo Manzini</a></p>
+                <p className="text-light"><small><strong>Working with Spring and React</strong><br />
+      Found me at LinkedIn: <a href="www.linkedin.com/in/leonardo-manzini" target="_blank" rel="noreferrer">Leonardo Manzini</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
