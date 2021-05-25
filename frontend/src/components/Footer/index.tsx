e55de6e37c4f2cfdf0585cd417e4ed60b825640e
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-25 20:53:23 -0300

    Adding some links and name changes

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <div className="container">
                 <p className="text-light">App developed by <a href="https://github.com/LeoManzini" target="_blank" rel="noreferrer">Leonardo Manzini</a></p>
                 <p className="text-light"><small><strong>Working with Spring and React</strong><br />
-      Found me at LinkedIn: <a href="www.linkedin.com/in/leonardo-manzini" target="_blank" rel="noreferrer">Leonardo Manzini</a></small></p>
+      Found me at LinkedIn: <a href="https://www.linkedin.com/in/leonardo-manzini" target="_blank" rel="noreferrer">Leonardo Manzini</a></small></p>
             </div>
         </footer>
     );
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">DSVendas</h1>
-                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
-                <hr />
-                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">ManziniSales</h1>
+                    <p className="lead">Analyze your sales performance from different perspectives</p>
+                    <hr />
+                    <p>This application consists of displaying a dashboard from data provided by a back end built with Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acess sales dashboard
+                    </Link>
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     );
 }
 
