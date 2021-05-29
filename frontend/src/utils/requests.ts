6031b2dc9d52d3ec09e722b14e16c026c4317eec
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-29 14:03:58 -0300

    Adding links descriptions

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://sales-leomanzini.herokuapp.com';
-//process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = 'https://sales-leomanzini.herokuapp.com'; //Heroku
+//export const BASE_URL = 'http://localhost:8080'; LocalHost
