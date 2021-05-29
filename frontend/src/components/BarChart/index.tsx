957899c32ba6651fb8e4b00cfce249de7560bf78
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-29 13:57:13 -0300

    Adding the object to the site see

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -52,7 +52,7 @@ function BarChart() {
 
             console.log(chartData);
         });
-    });
+    }, [chartData]);
 
     const options = {
         plotOptions: {
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,7 +24,7 @@ function DonutChart() {
     
             console.log(chartData);
         });
-    });
+    }, [chartData]);
     
     const options = {
         legend: {
